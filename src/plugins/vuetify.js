00e542bf53b7b5a2e31536415610a878d4f449dc
Jason <jdrames@myactv.net>
2021-03-14 13:44:58 -0400

    set default theme to dark mode

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+// setup vuetify to use the dark theme
+export default new Vuetify({
+    theme: {dark: true}
+});
