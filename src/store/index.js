64acb0163714f1241b688434c7a3c415b00cde82
Jason <jdrames@myactv.net>
2021-03-14 20:57:17 -0400

    updated state to better handle auth payload

--- src/store/index.js
+++ src/store/index.js
@@ -6,28 +6,28 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     token: localStorage.getItem("token") ? localStorage.getItem("token") : null,
-    name: localStorage.getItem("name") ? localStorage.getItem("name") : null
+    name: localStorage.getItem("name") ? localStorage.getItem("name") : null,
+    id: localStorage.getItem("id") ? localStorage.getItem("id") : null
   },
   mutations: {
-    setName(state, name){
-      state.name = name;      
+    setUser(state, payload){
+      state.id = payload?.user?.id;
+      state.name = payload?.user?.name;      
+      state.token = payload?.token;
     },
-    setToken(state, token){
-      state.token = token;      
-    }
   },
   actions: {
     doLogin(context, payload){
-      context.commit("setName", payload.name);
-      localStorage.setItem("name", payload.name);
-
-      context.commit("setToken", payload.token);
-      localStorage.setItem("token", payload.token);
+      context.commit("setUser", payload);
+      localStorage.setItem("id", payload?.user?.id);
+      localStorage.setItem("name", payload?.user?.name);      
+      localStorage.setItem("token", payload?.token);
     },
     doLogout(context){
-      context.commit("setName", null);
-      context.commit("setToken", null);
-      localStorage.clear();      
+      context.commit("setUser", null);      
+      localStorage.removeItem("id");
+      localStorage.removeItem("name");
+      localStorage.removeItem("token");
     }
   },
   modules: {
