94d3099c72cb2a40405f8c1c4d3d026d94d22baa
Jason <jdrames@myactv.net>
2021-03-14 21:05:42 -0400

    setup route guard to check auth status

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Store from '../store'
 
 Vue.use(VueRouter)
 
@@ -14,15 +15,33 @@ const routes = [
     path: '/getstarted',
     name: 'Get-Started',
     component: () => import(/* webpackChunkName: "getstarted" */ '../views/Getstarted.vue')    
-  },  
+  },
+  {
+    path: '/host',
+    name: 'Host-Game',
+    component: () => import(/* webpackChunkName: "hostgame" */ '../views/Hostgame.vue'),
+    meta:{
+      requiresAuth: true
+    }
+  }  
 ]
-
-
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+/** Basic router guard */
+router.beforeEach((to, from, next)=>{
+  // check if the page requires auth
+  if(to.meta?.requiresAuth == true){
+    if(!Store.state.token){
+      next({path: '/', replace: true});
+    }
+  }
+  // if not just load the next page
+  next();
+});
 
 export default router
