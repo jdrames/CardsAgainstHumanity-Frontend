2f21fa2efe7638efa042299fa0610def41f0877a
Jason <jdrames@myactv.net>
2021-03-15 01:50:29 -0400

    cardpacks mapped with their values from server

---
+++ src/assets/cardpacks.js
@@ -0,0 +1,35 @@
+/**
+ * This exports a list of game packs supported by the 
+ * the base game (0) is always included and should be 
+ * left out of this list.
+ */
+export default ([
+    {name: "The Red Box", value: 1, favorite: true},
+    {name: "The Blue Box", value: 2, favorite: true},
+    {name: "The Green Box", value: 3, favorite: true},
+    {name: "The Absurd Box", value: 4, favorite: false},
+    {name: "90's Nostalgia Pack", value: 5, favorite: true},
+    {name: "Fantasy Pack", value: 6, favorite: false},
+    {name: "World Wide Web Pack", value: 7, favorite: false},
+    {name: "Period Pack", value: 8, favorite: true},
+    {name: "Weed Pack", value: 9, favorite: false},
+    {name: "College Pack", value: 10, favorite: true},
+    {name: "CAH Saves America Pack", value: 11, favorite: false},
+    {name: "The Ass Pack", value: 12, favorite: false},
+    {name: "2000's Nostalgia Pack", value: 13, favorite: true},
+    {name: "The AI Pack", value: 14, favorite: false},
+    {name: "The Human Pack", value: 15, favorite: false},
+    {name: "2012 Holiday Pack", value: 16, favorite: false},    
+    {name: "2013 Holiday Pack", value: 17, favorite: false},
+    {name: "2014 Holiday Pack", value: 18, favorite: false},
+    {name: "The Jew Pack", value: 19, favorite: false},
+    {name: "Seasons Greetings Pack", value: 20, favorite: false},
+    {name: "Hidden Gems", value: 21, favorite: false},
+    {name: "Nerd Bundle", value: 22, favorite: true},
+    {name: "Pride Pack", value: 23, favorite: true},
+    {name: "The Dad Pack", value: 24, favorite: true},
+    {name: "Science Pack", value: 25, favorite: false},
+    {name: "Food Pack", value: 26, favorite: false},
+    {name: "Geek Pack", value: 27, favorite: false},
+    {name: "Sci-Fi Pack", value: 28, favorite: false}
+])
